827d59e6d066a702afc6389a1ab18472b8b83246
grubby <491096324@qq.com>
2015-07-10 10:46:29 +0800

    1111

---
+++ InitMainScene.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitMainScene : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		//TextAsset textAsset = Resources.Load( "Lua/ApplicationInit.lua", typeof(TextAsset)) as TextAsset;
+
+		//StackMgr.getState ().DoString (textAsset.text);
+		ApplicationInit.Start();
+	}
+
+
+}
